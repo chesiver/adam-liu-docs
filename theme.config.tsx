1cbdeaea60e65e1c77697c2629a154abbe2550f2
Yidong Liu <ljhchesiver@gmail.com>
2023-05-20 22:51:22 -0700

    churn

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-    logo: <span>Adam Liu - Personal Documentations</span>,
+    logo: <span>Adam(Yidong) Liu - Personal Documentations</span>,
     project: {
         link: 'https://github.com/shuding/nextra-docs-template',
     },
