553a13b49bb3fae3325cc751818ae3b5892e929b
adam.liu <adam.liu@bytedance.com>
2023-05-26 15:50:54 -0700

    add tl language section

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,34 @@
 import nextra from 'nextra'
 
+// https://github.com/shuding/nextra/pull/562
+// https://github.com/mike-lischke/vscode-antlr4/blob/master/syntaxes/antlr.json
+import antlrGrammar from './antlr/antlr.tmLanguage.json' assert { type: 'json' };
+const antlr = {
+    id: "antlr",
+    scopeName: 'source.antlr',
+    grammar: antlrGrammar,
+    aliases: ['antlr'],
+}
+import { getHighlighter, BUNDLED_LANGUAGES } from 'shiki';
+const rehypePrettyCodeOptions = {
+    getHighlighter: options => {
+        return getHighlighter({
+            ...options,
+            langs: [
+                ...BUNDLED_LANGUAGES,
+                antlr,
+            ],
+        });
+    },
+};
+
 const withNextra = nextra({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
+    theme: 'nextra-theme-docs',
+    themeConfig: './theme.config.tsx',
+    mdxOptions: {
+        rehypePrettyCodeOptions,
+    }
 })
+
 
 export default withNextra()
--- theme.config.tsx
+++ theme.config.tsx
@@ -13,6 +13,9 @@ const config: DocsThemeConfig = {
     footer: {
         text: 'Nextra Docs Template',
     },
+    sidebar: {
+        defaultMenuCollapseLevel: 3,
+    }
 }
 
 export default config
